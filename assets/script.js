bfb3ba45d15158bbbfba30cf78a29025918dc5e1
Ana Ea <aabad0464@gmail.com>
2021-06-15 14:21:58 -0600

    link script.js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Mobile Menu
+let burgerIcon = document.querySelector(`#burger`);
+let navbarMenu = document.querySelector(`#navLinks`);
+
+burgerIcon.addEventListener(`click`, () => {
+    navbarMenu.classList.toggle(`is-active`);
+});
